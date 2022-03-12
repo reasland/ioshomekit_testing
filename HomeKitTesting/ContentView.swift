363faf0bd46674b7551a8c52f6cd140888b720a1
Riley Easland <riley@eas.land>
2022-03-12 17:18:50 -0600

    Initial Commit

---
+++ HomeKitTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeKitTesting
+//
+//  Created by Riley Easland on 3/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeKitTesting/HomeKitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeKitTestingApp.swift
+//  HomeKitTesting
+//
+//  Created by Riley Easland on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeKitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
